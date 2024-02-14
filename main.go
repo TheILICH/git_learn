135439e08d7f53e69c7f2cd60a48253ee7eb511c
TheILICH <iliyasbek2003@gmail.com>
2024-02-14 12:05:05 +0600

    commit

--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	N := 100
+	p := make([]bool, N)
+
+	for i := 0; i < N; i++ {
+		p[i] = true
+	}
+
+	p[0], p[1] = false, false
+	for i := 0; i < N; i++ {
+		if !p[i] {
+			continue
+		}
+		for j := i * i; j < N; j += i {
+			p[j] = false
+		}
+	}
+
+	var res []int
+	for i := 0; i < N; i++ {
+		if p[i] {
+			res = append(res, i)
+		}
+	}
+
+	fmt.Printf("PRIMES BETWEEN 1 AND %d ARE => \n%v\n", N, res)
+
+
 }
